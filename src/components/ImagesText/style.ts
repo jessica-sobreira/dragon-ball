cf54cc5a24055b4c813b55428275380a0afbecf7
Jéssica Sobreira <117686537+jessica-sobreira@users.noreply.github.com>
2023-12-02 18:58:33 -0300

    Update style.ts

--- src/components/ImagesText/style.ts
+++ src/components/ImagesText/style.ts
@@ -22,6 +22,23 @@ export const Container2 = styled.section<{reversed?: boolean}>`
         font-weight: 300;
     }
 }
+
+@media screen and (min-width: 300px) and (max-width: 768px) {
+    
+    flex-direction: column;
+
+    .title {
+      font-size: 2.5em;
+      text-align: center;
+      margin-bottom: 20px; 
+    }
+
+    .content {
+      padding: 5%;
+      font-size: 1em; 
+    }
+  }
+
 `;
 
 export const ImageContainer = styled.img`
@@ -29,4 +46,9 @@ export const ImageContainer = styled.img`
     margin: 0;
     width: 50%;
     object-fit: cover;
+
+    @media screen and (min-width: 300px) and (max-width: 768px) {
+        width: 100vw;
+        height: auto;
+      }
 `;
