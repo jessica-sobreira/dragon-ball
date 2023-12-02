c166f1e616c9ca4774405076d554b78ebf0f6f4d
Jéssica Sobreira <117686537+jessica-sobreira@users.noreply.github.com>
2023-12-02 18:34:03 -0300

    Update style.ts

--- src/components/Icon/style.ts
+++ src/components/Icon/style.ts
@@ -2,21 +2,32 @@ import styled from "styled-components";
 
 export const FlexSection = styled.section`
     display: flex;
+    flex-direction: row;
     justify-content: space-around;
     background-color: lightyellow;
-    padding-top: 0;
+    padding: 20px 0;
+
+    
+
+    @media (min-width: 200px) {
+      flex-wrap: wrap;
+      align-items: flex-start; 
+      padding-top: 0;
+
+    }
 
 `;
 
 export const Container = styled.section`
-  padding: 3rem 0;
+  padding: 2rem 1rem; 
   margin: 0;
   color: black;
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
-  width: 30vw;
+  width: 100%;
+
 
 h3 {
     margin: 0;
@@ -27,6 +38,17 @@ p {
     font-size: 1.5rem;
     font-weight: 300;
 }
+
+@media (min-width: 768px) {
+  width: 30vw;
+  padding: 3rem 0;
+
+
+  h3, p {
+    text-align: left;
+  }
+} 
+
 `;
 
 export const IconContainer = styled.div`
@@ -34,4 +56,3 @@ export const IconContainer = styled.div`
         font-size: 5em;
         color: darkorange;
     }`;
-
